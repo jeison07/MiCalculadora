3564569194f56fac1363248503f4124a0bc31e18
jeison <jeison07777@gmail.com>
2022-04-30 16:48:04 -0500

    asignandole funciones a los botones del suma, resta, multiplicacion y divicion

--- app/src/main/java/com/example/micalculadora/MainActivity.kt
+++ app/src/main/java/com/example/micalculadora/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.example.micalculadora
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var num1: Int = 0
+    private var num2: Int = 0
+    private var operacion: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        unoButton.setOnClicklistener {numeroPresionado("1")}
+
+
+
+
+        unoButton.setOnClickListener {numeroPresionado("1")}
         dosButton.setOnClickListener {numeroPresionado("2")}
         tresButton.setOnClickListener {numeroPresionado("3")}
         cuatroButton.setOnClickListener {numeroPresionado("4")}
@@ -22,8 +35,57 @@ class MainActivity : AppCompatActivity() {
         nueveButton.setOnClickListener {numeroPresionado("9")}
         ceroButton.setOnClickListener {numeroPresionado("0")}
 
+        sumaButton.setOnClickListener { operacionPrecionanada(SUMA) }
+        restaButton.setOnClickListener { operacionPrecionanada(RESTA) }
+        multiplicacionButton.setOnClickListener { operacionPrecionanada(MULTIPLICACION) }
+        dividicionButton.setOnClickListener { operacionPrecionanada(DIVICION) }
+
+
+
+        borrarButton.setOnClickListener {
+            num1 = 0
+            num2 = 0
+            resultadoTextView.text = "0"
+            operacion = NO_OPERACION
+        }
+
+        igualButton.setOnClickListener {
+            var resultado = when(operacion){
+                SUMA -> num1+num2
+                RESTA -> num1-num2
+                MULTIPLICACION -> num1*num2
+                DIVICION -> num1/num2
+                else-> 0
+            }
+            resultadoTextView.text = resultado.toString()
+        }
+
+
+
+
     }
     private fun numeroPresionado(digito: String){
-        resultadoTextView.text = "${resultadoTextView}$digito"
+        resultadoTextView.text = "${resultadoTextView.text}$digito"
+
+        if (operacion == NO_OPERACION){
+            num1 = resultadoTextView.text.toString().toInt()
+        }else{
+            num2 = resultadoTextView.text.toString().toInt()
+        }
+    }
+    private fun operacionPrecionanada(operacion: Int) {
+        this.operacion = operacion
+
+
+
+        resultadoTextView.text = "0"
+
+    }
+       companion object{
+           const val SUMA = 1
+           const val RESTA = 2
+           const val MULTIPLICACION = 3
+           const val DIVICION = 4
+           const val NO_OPERACION = 0
     }
 }
