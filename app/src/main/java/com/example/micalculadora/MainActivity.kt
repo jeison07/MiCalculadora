7e04976e986a5a43f7fc1624821b168dd802fda6
jeison <jeison07777@gmail.com>
2022-04-30 07:36:46 -0500

    asignandole funciones a los botones del 1 a 0

--- app/src/main/java/com/example/micalculadora/MainActivity.kt
+++ app/src/main/java/com/example/micalculadora/MainActivity.kt
@@ -3,10 +3,27 @@ package com.example.micalculadora
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        unoButton.setOnClicklistener {numeroPresionado("1")}
+        dosButton.setOnClickListener {numeroPresionado("2")}
+        tresButton.setOnClickListener {numeroPresionado("3")}
+        cuatroButton.setOnClickListener {numeroPresionado("4")}
+        cincoButton.setOnClickListener {numeroPresionado("5")}
+        seisButton.setOnClickListener {numeroPresionado("6")}
+        sieteButton.setOnClickListener {numeroPresionado("7")}
+        ochoButton.setOnClickListener {numeroPresionado("8")}
+        nueveButton.setOnClickListener {numeroPresionado("9")}
+        ceroButton.setOnClickListener {numeroPresionado("0")}
+
+    }
+    private fun numeroPresionado(digito: String){
+        resultadoTextView.text = "${resultadoTextView}$digito"
     }
 }
